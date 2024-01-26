1a72f0a67491310e4a4ec7f635b4b6a18f55e8ba
alysson-melo <143971749+alysson-melo@users.noreply.github.com>
2024-01-26 01:07:31 -0300

    v2.1

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function carregar() {
+window.onload = function () {
     let msg = document.getElementById('msg')
     let img = document.getElementById('img')
     let data = new Date()
-    let hora = data.getHours() 
+    let hora = data.getHours()
     let min = data.getMinutes()
     msg.innerHTML = `Agora são: ${hora} horas e ${min} minutos.`
 
     if (hora >= 0 && hora < 12) {
-        img.src = 'imgs/manha.jpg' 
+        img.src = 'imgs/manha.jpg'
+        document.body.style.background = '#F2BF5E'
     } else if (hora >= 12 && hora < 18) {
         img.src = 'imgs/tarde.jpg'
+        document.body.style.background = '#F25C05'
     } else {
         img.src = 'imgs/noite.jpg'
+        document.body.style.background = '#3C4C59'
     }
 }
 
